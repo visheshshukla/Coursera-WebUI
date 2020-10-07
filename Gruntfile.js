22d5978456fd168f0116cd6bf0321a550d86e711
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-10-08 01:14:36 +0530

    grunt part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,9 +15,31 @@ module.exports = function (grunt) {
                     'css/styles.css': 'css/styles.scss'
                 }
             }
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./"
+                    }
+                }
+            }
         }
     });
 
     grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
 
 };
